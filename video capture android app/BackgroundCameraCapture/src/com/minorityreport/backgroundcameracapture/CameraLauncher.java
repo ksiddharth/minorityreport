2cc1d4fc016fc137d982fa75208a3f899d1ce1bd
ksiddharth <siddharth.manikandan@gmail.com>
2015-06-28 12:34:12 +0530

    Video capture project setup
    Initial checkin for video capture android project

---
+++ video capture android app/BackgroundCameraCapture/src/com/minorityreport/backgroundcameracapture/CameraLauncher.java
@@ -0,0 +1,12 @@
+package com.minorityreport.backgroundcameracapture;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraLauncher extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
